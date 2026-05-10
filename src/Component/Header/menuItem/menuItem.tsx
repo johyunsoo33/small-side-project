9066f2505c95ba011c40ce7896f3e7905ddd206c
J.H.S_0616 <johyunsoo369@naver.com>
2026-05-10 17:05:47 +0900

    메뉴 아이템에 링크 추가 및 메뉴 리스트 구조 개선

--- src/Component/Header/menuItem/menuItem.tsx
+++ src/Component/Header/menuItem/menuItem.tsx
@@ -2,13 +2,14 @@ import Link from "next/link";
 
 interface menuContentProps {
   content: string;
+  targetLink: string;
 }
 
-export default function MenuItem({ content }: menuContentProps) {
+export default function MenuItem({ content, targetLink }: menuContentProps) {
   return (
     <>
       <li className="border-b-blue-500 border-1 p-3" id="menuItem">
-        <Link href={""}>{content}</Link>
+        <Link href={`/${targetLink}`}>{content}</Link>
       </li>
     </>
   );
--- src/Component/Header/menuList/menuList.tsx
+++ src/Component/Header/menuList/menuList.tsx
@@ -1,16 +1,71 @@
-import Image from "next/image";
+"use client";
+import { useRef, useState } from "react";
 import MenuItem from "../menuItem/menuItem";
 
 export default function MenuList() {
+  const topLineRef = useRef<HTMLDivElement>(null);
+  const botLineRef = useRef<HTMLDivElement>(null);
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleClick = () => {
+    const topLine = topLineRef.current;
+    const botLine = botLineRef.current;
+
+    if (topLine && botLine) {
+      setIsOpen(!isOpen);
+
+      if (!isOpen) {
+        // X 모양으로: 둘 다 중앙으로 이동 + 회전
+        topLine.className =
+          "absolute top-1/2 left-0 w-full h-0.5 bg-white rotate-45 transition-all";
+        botLine.className =
+          "absolute top-1/2 left-0 w-full h-0.5 bg-white -rotate-45 transition-all";
+      } else {
+        // 원래 모양으로: 위아래로 이동 + 회전 해제
+        topLine.className =
+          "absolute top-1/2 left-0 w-full h-0.5 bg-white -translate-y-1 transition-all";
+        botLine.className =
+          "absolute top-1/2 left-0 w-full h-0.5 bg-white translate-y-1 transition-all";
+      }
+    }
+  };
+
   return (
-    <>
-      <div className="border-amber-600 border-1">
-        <Image src={""} alt="" />
-        <ul className="w-fit">
-          <MenuItem content="일정"></MenuItem>
-          <MenuItem content="메모"></MenuItem>
-        </ul>
+    <div className="border-amber-600 border-1">
+      <div className="relative w-6 h-6" id="menuIcon" onClick={handleClick}>
+        <div
+          ref={topLineRef}
+          className="absolute top-1/2 left-0 w-full h-0.5 bg-white -translate-y-1 origin-center"
+          id="menuIconTopLine"
+        ></div>
+        <div
+          ref={botLineRef}
+          className="absolute top-1/2 left-0 w-full h-0.5 bg-white translate-y-1 origin-center"
+          id="menuIconBotLine"
+        ></div>
+        {/* <div className="absolute top-1/2 left-0 w-full h-0.5 bg-white rotate-45 origin-center"></div>
+        <div className="absolute top-1/2 left-0 w-full h-0.5 bg-white -rotate-45 origin-center"></div> */}
       </div>
-    </>
+
+      {isOpen ? (
+        <ul className="flex justify-between">
+          {/* 왼쪽 메인 기능 */}
+          <div className="flex">
+            <MenuItem content="일정" targetLink="mySchedule" />
+            <MenuItem content="메모" targetLink="myMemo" />
+            <MenuItem content="할 일" targetLink="myTasks" />
+          </div>
+
+          {/* 오른쪽 유틸리티 */}
+          <div className="flex">
+            <MenuItem content="최근 문서" targetLink="recent" />
+            <MenuItem content="즐겨찾기" targetLink="favorites" />
+            <MenuItem content="휴지통" targetLink="trash" />
+          </div>
+        </ul>
+      ) : (
+        <></>
+      )}
+    </div>
   );
 }
