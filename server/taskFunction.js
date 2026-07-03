4a0244051f4bf947958847425bdad8fcdd9083cf
J.H.S_0616 <johyunsoo369@naver.com>
2026-07-03 17:02:15 +0900

    ✨ API 엔드포인트 경로 수정: 작업 관련 API 경로를 명확하게 변경하여 가독성 향상

--- server/taskFunction.js
+++ server/taskFunction.js
@@ -13,19 +13,19 @@ const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
   console.log(`서버가 ${PORT}번으로 시작하였습니다.`);
 });
-app.get("/api/tasks", async (req, res) => {
+app.get("/api/tasks/get", async (req, res) => {
   const tasks = await find("Task");
   res.send(tasks);
 });
-app.post("/api/tasks", async (req, res) => {
+app.post("/api/tasks/post", async (req, res) => {
   const r = await insert("Task", req.body.param);
   res.send(r);
 });
-app.put("/api/tasks/:_id", async (req, res) => {
+app.put("/api/tasks/put/:_id", async (req, res) => {
   const r = await update("Task", req.body.param, req.params._id);
   res.send(r);
 });
-app.delete("/api/tasks/:_id", async (req, res) => {
+app.delete("/api/tasks/delete/:_id", async (req, res) => {
   const r = await deleteByID("Task", req.params._id);
   res.send(r);
 });
