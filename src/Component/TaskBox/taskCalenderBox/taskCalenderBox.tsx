bc35280c5b31b3df987153d2b19e2fde3cf8f9d5
J.H.S_0616 <johyunsoo369@naver.com>
2026-07-09 19:20:52 +0900

    ✨ TaskCalenderBox 컴포넌트 수정: 날짜별 작업 목록 표시 기능 추가

--- src/Component/TaskBox/taskCalenderBox/taskCalenderBox.tsx
+++ src/Component/TaskBox/taskCalenderBox/taskCalenderBox.tsx
@@ -1,11 +1,25 @@
+import { TaskProps } from "@/src/types/addTaskType";
+
 export interface CalendarDate {
   day: number;
+  date: Date;
   isCurrentMonth: boolean;
   isPrevMonth: boolean;
 }
 
 interface TaskCalenderBoxProps {
   calendar: CalendarDate[];
+}
+
+function toDateKey(date: Date) {
+  return `${date.getFullYear()}-${String(date.getMonth() + 1).padStart(2, "0")}-${String(date.getDate()).padStart(2, "0")}`;
+}
+
+function getTasksForDate(taskList: TaskProps[], date: Date) {
+  const dateKey = toDateKey(date);
+  return taskList.filter(
+    (task) => task.startDate <= dateKey && dateKey <= task.endDate,
+  );
 }
 
 export default function TaskCalenderBox({
@@ -21,16 +35,31 @@ export default function TaskCalenderBox({
           </div>
         ))}
 
-        {calendar.map((date, index) => (
-          <div
-            key={index}
-            className={`min-w-28 min-h-28 pb-4 pl-1 pt-1 ${
-              date.isCurrentMonth ? "text-white" : "text-gray-400"
-            } border-1 border-white rounded-xl`}
-          >
-            {date.day}
-          </div>
-        ))}
+        {calendar.map((date, index) => {
+          const tasksForDate = getTasksForDate(taskList, date.date);
+
+          return (
+            <div
+              key={index}
+              className={`min-w-28 min-h-28 pb-4 pl-1 pt-1 ${
+                date.isCurrentMonth ? "text-white" : "text-gray-400"
+              } border-1 border-white rounded-xl`}
+            >
+              <div>{date.day}</div>
+              <div className="flex flex-col gap-1 mt-1">
+                {tasksForDate.map((task) => (
+                  <div
+                    key={task._id}
+                    className="truncate rounded bg-[#4F5DFF] px-1 text-xs text-white"
+                    title={task.title}
+                  >
+                    {task.title}
+                  </div>
+                ))}
+              </div>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
