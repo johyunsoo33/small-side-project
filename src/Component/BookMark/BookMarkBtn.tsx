3226fa17765784b4f84518d3623e5a4b89ac7dfd
J.H.S_0616 <johyunsoo369@naver.com>
2026-08-29 17:42:08 +0900

    ✨ 북마크 버튼 컴포넌트 개선: props 추가 및 클릭 핸들러 수정

--- src/Component/BookMark/BookMarkBtn.tsx
+++ src/Component/BookMark/BookMarkBtn.tsx
@@ -1,22 +1,33 @@
 "use client";
 import Image from "next/image";
-import { useState } from "react";
+import { MouseEvent, useState } from "react";
 
-export default function BookMarkBtn() {
-  const [isBookMarked, setIsBookMarked] = useState(false);
+interface BookMarkBtnProps {
+  id: string;
+  isBookmarked: boolean; // ① 서버 값을 초기값으로
+  onClick: (id: string) => void;
+}
+export default function BookMarkBtn({
+  id,
+  isBookmarked,
+  onClick,
+}: BookMarkBtnProps) {
+  const [marked, setMarked] = useState(isBookmarked);
 
-  const handleBookMarkClick = () => {
-    setIsBookMarked(!isBookMarked);
+  const handleClick = (event: MouseEvent<HTMLButtonElement>) => {
+    event.stopPropagation(); // ② 카드 클릭까지 번지지 않게
+    setMarked(!marked);
+    onClick(id);
   };
   return (
-    <button onClick={handleBookMarkClick}>
+    <button type="button" onClick={handleClick}>
       <Image
         src={
-          isBookMarked
+          marked
             ? "/Icons/bookmark_click.svg"
             : "/Icons/bookmark_none_click.svg"
         }
-        alt={isBookMarked ? "북마크 클릭후" : "북마크 클릭전"}
+        alt={marked ? "북마크 클릭후" : "북마크 클릭전"}
         width={24}
         height={24}
       />
