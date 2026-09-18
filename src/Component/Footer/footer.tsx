7f8d33cae076569fce83fa321266d9e42b119331
J.H.S_0616 <johyunsoo369@naver.com>
2026-09-18 21:09:07 +0900

    ✨ 주석 추가: Footer 및 Header 컴포넌트에 대한 설명 주석 추가, 코드 가독성 향상

--- src/Component/Footer/footer.tsx
+++ src/Component/Footer/footer.tsx
@@ -1,3 +1,4 @@
+// [공통] 페이지 하단 (아직 비어 있음)
 export default function Footer() {
   return (
     <>
--- src/Component/Header/header.tsx
+++ src/Component/Header/header.tsx
@@ -1,5 +1,6 @@
 import MenuList from "./menuList/menuList";
 
+// [공통] 페이지 상단. 좌측 메뉴를 담는다.
 export default function Header() {
   return (
     <>
